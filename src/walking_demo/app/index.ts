108fca80aeadadcacaa04293cd3304cf172d3246
Craig Brett <craigbrett17@aol.com>
2018-08-01 23:14:16 +0100

    Got a basic rotation with howler working

--- src/walking_demo/app/index.ts
+++ src/walking_demo/app/index.ts
@@ -3,23 +3,16 @@ declare function require(s: string): string
 import { Key } from 'ts-keycode-enum';
 import './main.css'
 import keyDownEventTracker from '../../engine/key-down-tracker'
-import { PointOfView } from '../../engine/point-of-view'
-import { SoundSource } from '../../engine/sound-source'
-import sono from 'sono'
-import 'sono/effects'
-import 'sono/utils'
 import '../sounds/fireplace.wav'
 
-import { Howl, Howler } from 'howler';
-
-const pov = new PointOfView()
-pov.activate()
-sono.panner.defaults.maxDistance = 10
+import { Howl, Howler } from 'howler'
 
 const outputElement = document.getElementById("screen-output")
 let started = false
 const mapDistance = 50
 let gameLoopHandle: number
+let dir = 0
+const circle: number = Math.PI * 2;
 
 keyDownEventTracker(document.getElementById("app-area"), 250, ev => {
   if (!started && ev.which == Key.Enter) {
@@ -29,7 +22,6 @@ keyDownEventTracker(document.getElementById("app-area"), 250, ev => {
   }
 })
 
-let sound
 function startGame(): void {
   started = true
   
@@ -38,10 +30,14 @@ function startGame(): void {
 
   // give a little context
   outputElement.innerText = "You open your eyes. You find yourself in a spacious wooden hall. A fire crackles merrily in the harth. You may wander round and explore"
-  sound = new SoundSource(0, 5, -1).loadAndPlay({
-    src: 'sounds/fireplace.wav',
+  let fireplaceSound = new Howl({
+    autoplay: true,
     loop: true,
-  })
+    src: '../sounds/fireplace.wav'
+  });
+  fireplaceSound.pos(10, 0, -0.5);
+  Howler.pos(0, 0, -0.5)
+  rotate(Math.PI * 0);
 }
 
 function gameLoop() {
@@ -51,15 +47,23 @@ function gameLoop() {
 function inGameKeyPressHandler(ev: KeyboardEvent): void {
   switch (ev.which) {
     case Key.LeftArrow:
-      pov.rotateLeft(45)
-      console.log("Orientation: ", pov.OrientationX, pov.OrientationY, pov.OrientationZ)
+      rotate(-1)
       break
     case Key.RightArrow:
-      pov.rotateRight(45)
-      console.log("Orientation: ", pov.OrientationX, pov.OrientationY, pov.OrientationZ)
+      rotate(1)
       break
     case Key.UpArrow:
-      // walk forward
       break
   }
+}
+
+function rotate(angle: number): void {
+  dir = (dir + angle + circle) % circle;
+
+  // Calculate the rotation vector and update the orientation of the listener.
+  var x = Math.cos(dir);
+  var y = 0;
+  var z = Math.sin(dir);
+  Howler.orientation(x, y, z, 0, 1, 0);
+  outputElement.innerText = dir + " degrees, facing " + x + "," + y + "," + z
 }
